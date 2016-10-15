56967de66375b20e8a8a04425723b57134aa7cd4
shubham9411 <shubham9411@gmail.com>
2016-10-16 02:54:26 +0530

    Therapy option added

--- cc_wpmp_teams.php
+++ cc_wpmp_teams.php
@@ -29,55 +29,77 @@ if( ! function_exists('cc_wpmp_register_members')){
 		register_post_type( 'members', $args );
 	}
 }
-if(!function_exists('cc_create_team_metabox')){
-	function cc_create_team_metabox(){
-		add_meta_box( 'cc_team_details','Details','cc_team_metabox','members');
+if(!function_exists('cc_create_member_metabox')){
+	function cc_create_member_metabox(){
+		add_meta_box( 'cc_member_details','Details','cc_member_metabox','members');
 	}
 }
-if(!function_exists('cc_team_metabox')){
-	function cc_team_metabox($post){
+if(!function_exists('cc_member_metabox')){
+	function cc_member_metabox($post){
 		$values = get_post_custom( $post->ID );
-		$designation = isset($values['cc_team_designation']) ? esc_attr($values['cc_team_designation'][0]) : '';
-		$email = isset($values['cc_team_mail']) ? esc_attr($values['cc_team_mail'][0]) : '';
-		wp_nonce_field( 'cc_team_nonce', 'metabox_nonce' );
+		$designation = isset($values['cc_member_designation']) ? esc_attr($values['cc_member_designation'][0]) : '';
+		$email = isset($values['cc_member_mail']) ? esc_attr($values['cc_member_mail'][0]) : '';
+		$therapy = isset($values['cc_member_therapy']) ? esc_attr($values['cc_member_therapy'][0]) : '';
+		$therapies = array('Physical Therapists','Speech Therapists','Occupational Therapists');
+		wp_nonce_field( 'cc_member_nonce', 'metabox_nonce' );
 		?>
 		<div class="row">
 			<div class="form-group">
-				<label for="cc_team_designation" class="col-xs-4 control-label">Designation</label>
+				<label for="cc_member_therapy" class="col-xs-4 control-label">Therapy</label>
 				<div class="col-xs-8">
-					<input type="text" class="form-control col-xs-2" name="cc_team_designation" id="cc_team_designation" value="<?php echo $designation; ?>" />
+					<select name="cc_member_therapy">
+					<?php
+					foreach($therapies as $therapy_item){
+					?>
+						<option value="<?php echo $therapy_item;?>" <?php if($therapy==$therapy_item){echo 'selected';}?>><?php echo $therapy_item;?></option>
+					<?php
+					}
+					?>
+					</select>
 				</div>
 			</div>
 			<div class="form-group">
-				<label for="cc_team_mail" class="col-xs-4 control-label">Email</label>
+				<label for="cc_member_designation" class="col-xs-4 control-label">Designation</label>
 				<div class="col-xs-8">
-					<input type="email" class="form-control col-xs-2" name="cc_team_mail" id="cc_team_mail" value="<?php echo $email; ?>" />
+					<input type="text" class="form-control col-xs-2" name="cc_member_designation" id="cc_member_designation" value="<?php echo $designation; ?>" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="cc_member_mail" class="col-xs-4 control-label">Email</label>
+				<div class="col-xs-8">
+					<input type="email" class="form-control col-xs-2" name="cc_member_mail" id="cc_member_mail" value="<?php echo $email; ?>" />
 				</div>
 			</div>
 		</div>
 		<?php
 	}
 }
-if(!function_exists('cc_team_save')){
-	function cc_team_save( $post_id ) {
+if(!function_exists('cc_member_save')){
+	function cc_member_save( $post_id ) {
 		if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
 			return $post_id;
-		if( !isset( $_POST['metabox_nonce'] ) || !wp_verify_nonce( $_POST['metabox_nonce'], 'cc_team_nonce' ) )
+		if( !isset( $_POST['metabox_nonce'] ) || !wp_verify_nonce( $_POST['metabox_nonce'], 'cc_member_nonce' ) )
 			return $post_id;
 
 		if( !current_user_can( 'edit_post' ) )
 			return $post_id;
 
-		if ( isset( $_POST['cc_team_designation'] ) ) {
-			if( $_POST['cc_team_designation'] != '') {
-				$designation = $_POST['cc_team_designation'];
-				update_post_meta( $post_id, 'cc_team_designation', $designation);
+		if ( isset( $_POST['cc_member_designation'] ) ) {
+			if( $_POST['cc_member_designation'] != '') {
+				$designation = $_POST['cc_member_designation'];
+				update_post_meta( $post_id, 'cc_member_designation', $designation);
 			}
 		}
-		if ( isset( $_POST['cc_team_mail'] ) ) {
-			if( $_POST['cc_team_mail'] != '') {
-				$email = $_POST['cc_team_mail'];
-				update_post_meta( $post_id, 'cc_team_mail', $email);
+		if ( isset( $_POST['cc_member_mail'] ) ) {
+			if( $_POST['cc_member_mail'] != '') {
+				$email = $_POST['cc_member_mail'];
+				update_post_meta( $post_id, 'cc_member_mail', $email);
+			}
+		}
+		if ( isset( $_POST['cc_member_therapy'] ) ) {
+			if( $_POST['cc_member_therapy'] != '') {
+				$therapy = $_POST['cc_member_therapy'];
+				update_post_meta( $post_id, 'cc_member_therapy', $therapy);
 			}
 		}
 	}
--- templates/page-teams.php
+++ templates/page-teams.php
@@ -8,12 +8,13 @@ get_header();
 		</div>
 		<div>
 			<p class="text-center">
-				Our practice specializes in the treatment of developmental disabilities and<br> childhood disorders affecting the way children function and communicate in<br> their world.<br>
-				Our licensed physical therapists, occupational therapists, and<br> speech-language pathologists are trained specifically in the treatment of<br> infants, children and adolescents. In our practice it is our goal to help each<br> and every child exceed expectations on a daily basis
+				Our practice specializes in the treatment of developmental disabilities and childhood disorders affecting the way children function and communicate in their world.<br><br>
+				Our licensed physical therapists, occupational therapists, and speech-language pathologists are trained specifically in the treatment of infants, children and adolescents. In our practice it is our goal to help each and every child exceed expectations on a daily basis
 			</p>
 		</div>
 	</div>
 </div>
+<div class="gap-div"></div>
 <div class="row">
 	<div class="col-xs-offset-1 col-xs-10">
 		<?php
@@ -24,59 +25,83 @@ get_header();
 			$values = get_post_custom(get_the_id());
 			$designation = isset($values['cc_member_designation']) ? esc_attr($values['cc_member_designation'][0]) : '';
 			$email = isset($values['cc_member_mail']) ? esc_attr($values['cc_member_mail'][0]) : '';
+			$therapy = isset($values['cc_member_therapy']) ? esc_attr($values['cc_member_therapy'][0]) : '';
 		?>
-		<div class="mediumpurple-color">
-			<div class="team-heading font-white row">
-				<div class="col-xs-7">
-					<h2 class="inline-block-text"><?php the_title();?></h2>
-					<h3 class="inline-block-text"><i><?php echo $designation; ?></i></h3>
-				</div>
-				<div class="col-xs-5">
-					<h3 class="pull-right"><?php echo $email;?></h3>
+		<div class="member">
+			<div class="col-xs-offset-1 col-md-offset-3 col-xs-10 col-md-6">
+				<div class="kidprint text-center">
+					<h1><?php echo $therapy;?></h1>
 				</div>
 			</div>
-		</div>
-		<div class="full-border-purple">
-			<div class="gap-top">
-				<div class="row">
-					<div class="col-xs-4">
-						<?php echo get_the_post_thumbnail(0,'medium','class=img-responsive');?>
+			<div class="clearfix"></div>
+			<div class="gap-xs"></div>
+			<div class="mediumpurple-color">
+				<div class="team-heading font-white row">
+					<div class="col-xs-7">
+						<h2>
+						<p class="inline-block-text member-title"><?php the_title();?></p>
+						<p class="inline-block-text member-designation"><i><?php echo $designation; ?></i></p>
+						</h2>
 					</div>
-					<div class="col-xs-8">
-						<p class="excerpt">
-							<?php echo get_the_content();?>
-						</p>
+					<div class="col-xs-5">
+						<h2 class="pull-right">
+						<a href="mailto:Opportunities@SpecialNeedsPT.com">
+							<p class="member-title font-white">
+								<?php echo $email;?>
+							</p>
+						</a>
+						</h2>
 					</div>
 				</div>
 			</div>
+			<div class="full-border-purple">
+				<div class="gap-top">
+					<div class="row">
+						<div class="col-xs-4">
+							<?php echo get_the_post_thumbnail(0,'medium','class=img-responsive');?>
+						</div>
+						<div class="col-xs-8">
+							<p class="excerpt">
+								<?php echo get_the_content();?>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="gap-top"></div>
 		</div>
-		<div class="gap-top"></div>
 		<?php
 		}
 		?>
-		<div class="mediumpurple-color">
-			<div class="team-heading font-white row">
-				<div class="col-xs-3">
-					<h2 class="inline-block-text">You</h2>
-				</div>
-				<div class="">
-					<h3 class="pull-right">Email Resume at: Opportunities@SpecialNeedsPT.com</h3>
-				</div>
-			</div>
-		</div>
-		<div class="full-border-purple col-xs-12">
-			<div class="gap-top">
-				<div class="col-xs-4 graphite-bg your-image">
-					<h1 class="text-center">YOU</h1>
-				</div>
-				<div class="col-xs-8">
-					<p class="excerpt">
-						Special Needs Pediatric Therapy Services will consider licensed physical and occupational therapists with a minimum of one year pediatric experience. NDT & SI training is highly favored.
-					</p>
+		<div class="member">
+			<div class="mediumpurple-color col-xs-12">
+				<div class="team-heading font-white	">
+					<h2 class="inline-block-text"><p class="member-title">You</p></h2>
+					<h2 class="pull-right">
+					<a href="mailto:Opportunities@SpecialNeedsPT.com">
+						<p class="member-title font-white">
+							Email Resume at: Opportunities@SpecialNeedsPT.com
+						</p>
+					</a>
+					</h2>
 				</div>
 			</div>
+			<div class="full-border-purple col-xs-12">
+				<div class="gap-top">
+					<div class="row">
+						<div class="col-xs-4 graphite-bg your-image">
+							<h1 class="text-center">YOU</h1>
+						</div>
+						<div class="col-xs-8">
+							<p class="excerpt">
+								Special Needs Pediatric Therapy Services will consider licensed physical and occupational therapists with a minimum of one year pediatric experience. NDT & SI training is highly favored.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="gap-top"></div>
 		</div>
-		<div class="gap-top"></div>
 	</div>
 </div>
 <div class="gap-top"></div>

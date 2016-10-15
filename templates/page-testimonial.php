54380c32962cc275c48ff782649246912aaf638b
shubham9411 <shubham9411@gmail.com>
2016-10-16 01:10:47 +0530

    responsiveness testimonial

--- templates/page-testimonial.php
+++ templates/page-testimonial.php
@@ -17,7 +17,7 @@ get_header();
 		<h1 class="row"><?php echo get_the_title(); ?></h1>
 		<hr class="hr-purple">
 		<div class="row">
-			<div class="col-xs-3">
+			<div class="col-xs-3 full-width-xs thumbnail-testimonial">
 				<?php 
 				if(has_post_thumbnail($loop->post_id)){
 					echo get_the_post_thumbnail( 0, 'medium' , 'class=img-responsive' );
@@ -27,7 +27,7 @@ get_header();
 				}
 				?>
 			</div>
-			<div class="col-xs-9">
+			<div class="col-xs-9 full-width-xs">
 				<p class="excerpt"><?php echo get_the_content();?></p>
 			</div>
 		</div>
